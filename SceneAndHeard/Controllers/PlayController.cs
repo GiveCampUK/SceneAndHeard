0f75f6b9232c896f5327dc4cf71a9fd20cb3d4d2
unknown <daphne@cupcake.(none)>
2011-10-22 11:53:18 +0100

    skeleton project

---
+++ SceneAndHeard/Controllers/PlayController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SceneAndHeard.Controllers
+{
+    public class PlayController : BaseController
+    {
+
+    }
+
+    public class BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateView()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateView()
+        {
+            return View();
+        }
+
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteView()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ SceneAndHeard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SceneAndHeard.MvcApplication" Language="C#" %>
