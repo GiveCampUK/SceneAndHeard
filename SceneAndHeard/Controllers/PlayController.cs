40a50d1faeaf216df7dfd58f254fe6d81d8e8450
elena <elena.rocks@gmail.com>
2011-10-22 13:37:31 +0100

    Created Play controller and view for displaying plays

--- SceneAndHeard/Controllers/PlayController.cs
+++ SceneAndHeard/Controllers/PlayController.cs
@@ -2,51 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace SceneAndHeard.Controllers
 {
     public class PlayController : BaseController
     {
 
-    }
-
-    public class BaseController
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult CreateView()
-        {
-            return View();
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        public ActionResult UpdateView()
-        {
-            return View();
-        }
-
-
-        public ActionResult Update()
-        {
-            return View();
-        }
-
-        public ActionResult DeleteView()
-        {
-            return View();
-        }
-
-        public ActionResult Delete()
-        {
-            return View();
-        }
     }
 }
