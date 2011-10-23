db3ce6697980315e790f0ea8d09a5bd6b16046eb
dhendo <dhendo@gmail.com>
2011-10-23 07:55:12 +0100

    Added more details for production, performance

---
+++ SceneCrm.Entities/Production.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SceneCrm.Entities
+{
+    public partial class Production
+    {
+        /// <summary>
+        /// All volunteers involved in a production - inc each performance
+        /// </summary>
+        public Dictionary<Volunteer, Job> AllAssociatedVolunteers
+        {
+            get
+            {
+                var v = ProductionVolunteers.ToDictionary(productionVolunteer => productionVolunteer.Volunteer, productionVolunteer => productionVolunteer.Job);
+
+                foreach (var playvol in Plays.SelectMany(play => play.PlayVolunteers))
+                {
+                    v.Add(playvol.Volunteer, playvol.Job);
+                }
+                return v;
+            }
+        }
+
+
+        public List<Student> AssociatedStudents
+        {
+            get
+            {
+                return Plays.Select(play => play.Student).ToList();
+            }
+        }
+    }
+}
