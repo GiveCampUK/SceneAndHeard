a8655e6d2de125aecf69463e8c727a42a892201b
Gareth Visagie <gareth.visagie@gmail.com>
2011-10-23 01:57:16 +0100

    Refactored the Volunteer Allocation views to make it more usable

---
+++ SceneAndHeard.MvcWebApp/Support/InitialisesVolunteerAllocationView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SceneCrm.Entities;
+
+namespace SceneAndHeard.Support
+{
+    public class InitialisesVolunteerAllocationView
+    {
+        public void Initialise(dynamic viewBag, SceneCRM context)
+        {
+            Initialise(viewBag, context, new VolunteerAllocation[0]);
+        }
+
+        public void Initialise(dynamic viewBag, SceneCRM context, IEnumerable<VolunteerAllocation> currentVolunteers)
+        {
+
+            var volunteers = context.Volunteers
+                .ToArray()
+                .OrderBy(v => v.IsEligible)                
+                .Select(v => new { Id = v.VolunteerId.ToString(), Name = string.Format("{0}, {1}{2}", v.Surname, v.FirstName, v.IsEligible ? string.Empty : " (NOT ELIGIBLE)") })
+                .ToDictionary(v => v.Id, v => v.Name);
+
+            var jobs = context.Jobs
+                .Select(j => new { Id = j.JobId, Name = j.Description })
+                .ToDictionary(j => j.Id.ToString(), j => j.Name);
+
+            var currentVolunteerModel = currentVolunteers
+                .Select(cv => new { jobId = cv.JobId.ToString(), volunteerId = cv.VolunteerId.ToString(), notes = cv.Notes })
+                .OrderBy(cv => cv.jobId);
+
+            viewBag.CurrentVolunteers = currentVolunteerModel;
+            viewBag.PossibleVolunteers = volunteers;
+            viewBag.PossibleJobs = jobs;
+        }
+    }
+}
